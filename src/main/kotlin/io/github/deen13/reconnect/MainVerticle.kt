0ac175ef7cc516636d9693d125c64900dc7a0a89
deen13 <dierkes@smartsquare.de>
2020-06-09 21:37:06 +0200

    Initial commit

---
+++ src/main/kotlin/io/github/deen13/reconnect/MainVerticle.kt
@@ -0,0 +1,40 @@
+package io.github.deen13.reconnect
+
+import io.vertx.kotlin.coroutines.CoroutineVerticle
+import io.vertx.kotlin.mqtt.connectAwait
+import io.vertx.kotlin.mqtt.subscribeAwait
+import io.vertx.mqtt.MqttClient
+import io.vertx.mqtt.MqttClientOptions
+import kotlinx.coroutines.delay
+import java.lang.Exception
+
+class MainVerticle : CoroutineVerticle() {
+
+  private val mqttClientConfig by lazy { config.getJsonObject("mqtt").getJsonObject("client") }
+  private val mqttServerConfig by lazy { config.getJsonObject("mqtt").getJsonObject("server") }
+  private val client by lazy { MqttClient.create(vertx, MqttClientOptions(mqttClientConfig)) }
+
+  override suspend fun start() {
+    val client = connect()
+
+    client.subscribeAwait("test", 1)
+
+    client.publishHandler {
+      println("Received message on topic ${it.topicName()}")
+    }
+  }
+
+  private suspend fun connect(): MqttClient {
+    return try {
+      client.connectAwait(mqttServerConfig.getInteger("port"), mqttServerConfig.getString("hostname"))
+
+      client
+    } catch (e: Exception) {
+      println("Connection attempt failed. Waiting 5 seconds until the next reconnect.")
+      delay(5_000)
+      println("Reconnecting...")
+
+      connect()
+    }
+  }
+}
